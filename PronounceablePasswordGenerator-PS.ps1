ff9b544ec263a3bd1120fe0b611cd950c505937c
Mike <michaelhsu16@gmail.com>
2025-03-03 01:01:22 -0800

    Created base syllable chunk password generator

--- PronounceablePasswordGenerator-PS.ps1
+++ PronounceablePasswordGenerator-PS.ps1
@@ -1,3 +1,5 @@
+$random = New-Object System.Random
+
 function New-LetterPassword() {
     param (
         [int]$passwordLength = 8,
@@ -7,7 +9,6 @@ function New-LetterPassword() {
 
     $vowels = @("a", "e", "i", "o", "u", "y", "a", "e", "i") # Duplicates intentional
     $consonants = @("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "z", "b", "d", "l", "n", "r", "s", "t") # Duplicates intentional
-    $random = New-Object System.Random
     $passwordList = @()
 
     for ($j = 0; $j -lt $passwordsToGenerate; $j++) {
@@ -32,6 +33,59 @@ function New-LetterPassword() {
     $passwordList
 }
 
-New-LetterPassword -passwordLength 20 -passwordsToGenerate 10 -capitalizationChance 0.2
+
+
+$cvChunks = @("ba", "be", "bo", "da", "de", "do", "fa", "fe", "fi", "ka", "ke", "ko", "la", "le", "lo", "ma", "me", "mo", "pa", "pe", "po", "ta", "te", "to")
+$vcChunks = @("an", "ar", "el", "en", "im", "on", "ul", "um", "in", "ir", "or", "ur")
+$cvcChunks = @("bat", "bel", "dam", "far", "kel", "lom", "man", "pan", "tel", "vor", "zan")
+$vowels = @("a", "e", "i", "o", "u", "y")
+$length = 12
+
+$pronounceablePassword = ""
+$initialChunkRoll = $random.NextDouble()
+
+# Deciding initial chunk
+if($initialChunkRoll -lt 0.25) {
+    $pronounceablePassword += $cvChunks[$random.Next(0, $cvChunks.Length)]
+} elseif ($initialChunkRoll -lt 0.4) {
+    $pronounceablePassword += $vcChunks[$random.Next(0, $vcChunks.Length)]
+} elseif ($initialChunkRoll -lt 0.9) {
+    $pronounceablePassword += $cvcChunks[$random.Next(0, $cvcChunks.Length)]
+} else {
+    $pronounceablePassword += $vowels[$random.Next(0, $vowels.Length)]
+}
+
+# Keep adding chunks until the target password length is reached or exceeded, default is 8
+do {
+    $nextChunkRoll = $random.NextDouble()
+    if($pronounceablePassword[-1] -in $vowels) { # If the chunk ends in a vowel, the next chunk starts with a consonant
+        if($nextChunkRoll -lt 0.5) {
+            $pronounceablePassword += $cvChunks[$random.Next(0, $cvChunks.Length)]
+        } else {
+            $pronounceablePassword += $cvcChunks[$random.Next(0, $cvcChunks.Length)]
+        }
+    } else { # If the chunk ends in a consonant, the next chunk starts with a vowel
+        if($nextChunkRoll -lt 0.5) {
+            $pronounceablePassword += $vcChunks[$random.Next(0, $vcChunks.Length)]
+        } else {
+            $pronounceablePassword += $vowels[$random.Next(0, $vowels.Length)]
+        }
+    }
+
+} while ($pronounceablePassword.Length -lt $length)
+
+# Ensures the password is exactly the specified length by removing any extra characters
+if($pronounceablePassword.Length -gt $length) {
+    $pronounceablePassword = $pronounceablePassword.Substring(0, $length)
+}
+
+$pronounceablePassword
+
+<# TO DO:
+- Add random capitalization
+- Expand the array to include more interesting combinations of vowels and consonants
+- Turn the syllable chunk generator into a function
+
+#>
 
 
